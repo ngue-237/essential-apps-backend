92c01ba95fcd46d8f041d1c01390a7fd85aebf23
therese2808 <therese.wem@yahoo.com>
2023-03-20 11:41:09 +0100

    updated

--- sensor-service/models/nodeModel.js
+++ sensor-service/models/nodeModel.js
@@ -4,6 +4,7 @@ temp= require('./tempModel.js');
 npk= require('./npkModel.js');
 cam =require('./camModel.js');
 moisture= require('./moistureModel.js');
+user= require('./userModel.js');
 
 var Node= new Schema( 
  { 
@@ -12,6 +13,7 @@ var Node= new Schema(
     cam:[  { type : mongoose.Schema.Types.ObjectId, ref : 'camSensor' }],
     npk:[  { type : mongoose.Schema.Types.ObjectId, ref : 'npkSensor' }],
     moisture:[  { type: mongoose.Schema.Types.ObjectId, ref: 'moistureSensor' }],
+    user:[  { type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
  } 
 ); 
 
